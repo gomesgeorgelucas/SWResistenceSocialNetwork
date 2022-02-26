7494da9b4c485976dceeb8c4c368fb19acac9302
George Lucas Gomes <gomes.georgelucas@gmail.com>
2022-02-25 20:19:51 -0400

    Initial commit

---
+++ src/main/java/org/george/swresistencesocialnetwork/SwResistenceSocialNetworkApplication.java
@@ -0,0 +1,13 @@
+package org.george.swresistencesocialnetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwResistenceSocialNetworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwResistenceSocialNetworkApplication.class, args);
+    }
+
+}
